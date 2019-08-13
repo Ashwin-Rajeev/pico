4e5187d0c57ffcf3c88ce9151ed6d8300c3c796a
Ashwin-Rajeev <ashwinrajeev916@gmail.com>
2019-08-13 20:14:33 +0530

    Implement file saving and add images

--- content.js
+++ content.js
@@ -1,18 +1,44 @@
-function getSelectionText(){
+function getSelectionText() {
     var selectedText = ""
-    if (window.getSelection){
+    if (window.getSelection) {
         selectedText = window.getSelection().toString()
     }
     return selectedText
 }
 
+function saveTextAsFile(data) {
+    console.log(data)
+    var textToWrite = data;
+    var n = Math.floor(Date.now() / 1000)
+    var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+    var fileNameToSaveAs = n + ".txt";
+
+    var downloadLink = document.createElement("a");
+
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "My Hidden Link";
+    window.URL = window.URL || window.webkitURL;
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
+
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {
+    function (request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
             var thetext = getSelectionText()
-            if (thetext.length > 0){
-                console.log(thetext)
+            if (thetext.length > 0) {
+                // console.log(thetext)
+                saveTextAsFile(thetext)
+
             }
-      }
+        }
     }
-  );
+);
