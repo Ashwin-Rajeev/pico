db429c3d2e7fc3514c9f6b9759d5fbba41c9dc04
Ashwin-Rajeev <ashwinrajeev916@gmail.com>
2019-08-27 12:40:20 +0530

    Add new file type

--- constant.js
+++ constant.js
@@ -4,4 +4,7 @@ const TEXT_FILE_EXTENSION = '.txt';
 const WORD_DOC_MIME_TYPE = 'application/msword';
 const WORD_FILE_EXTENSION = '.doc';
 
+const OPEN_OFFICE_DOC_MIME_TYPE = 'application/vnd.oasis.opendocument.text';
+const OPEN_OFFICE_FILE_EXTENSION = '.odt';
+
 const HIGHLIGHT_COLOR = 'yellow';
